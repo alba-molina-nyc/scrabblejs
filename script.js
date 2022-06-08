8b2d590cb40be7b8114aefb2c61cb85f255ce264
Username <albavmolinanyc@gmail.com>
2022-06-08 17:05:51 -0400

    creates function to get data, it loads on console but not on html

--- script.js
+++ script.js
@@ -1,60 +1,39 @@
+let wordData, userInput;
 const $word = $("#word");
 const $definition = $("#definition");
-const $offensive = $("#offensive"); //this will be a boolean T or F
+const $offensive = $("#offensive");
 const $pronunciation = $("#pronunciation");
+const $input = $('input[type="text"]');
 
-/* 
-so the const variables above are constants and exist in the global scope
-# -> allows to select by id 
-$ -> is used through the jQuery install on html head
-*/
+$("form").on("submit", handleGetData);
+//www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=cec6fd31-d3a5-454a-a806-f997548b7cbf
+https: function handleGetData(event) {
+  const BASE_URL =
+    "https://www.dictionaryapi.com/api/v3/references/collegiate/json";
+  const API_KEY = "cec6fd31-d3a5-454a-a806-f997548b7cbf";
+  event.preventDefault();
+  userInput = $input.val();
+  $.ajax({
+    url: `${BASE_URL}/${userInput}?key=${API_KEY}`,
+  }).then(
+    (data) => {
+      wordData = data;
+      console.log(data);
+      render();
+    },
+    (error) => {
+      console.log("bad request", error);
+    }
+  );
+}
 
-// const API_KEY = "cec6fd31-d3a5-454a-a806-f997548b7cbf";
-const promise = $.ajax({
-  url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/ugly?key=cec6fd31-d3a5-454a-a806-f997548b7cbf",
-});
-
+function render() {
+  $word.text(wordData.Word);
+  $definition.text(wordData.Definition);
+  $offensive.text(wordData.Offensive);
+  $Pronunciation.text(wordData.Offensive);
+}
 /*
-$.ajax returns a "promise" object, which we are giong to save into the variable promise 
-so promise will be an object that will hold info about the akax request event
+   // calling preventDefault() on a 'submit' event will prevent a page refresh  
 
-the we the success and error callback
-
- */
-promise.then(
-  (data) => {
-    console.log(data);
-    $word.text(data.word);
-  },
-  (error) => {
-    console.log("bad request: ", error);
-  }
-);
-
-/* 
-const 
-maintain constant values and they can only be accessed within the block they have been declared & it cannot be updated or redeclared within the same scope like let
-
-
-
-
-let 
-is blocked scope, they live within {}, anything within curly braces is a block so a variable avail for use only inside the block it has been declared
-
-XXXXXX aka no go zone you cannot re-declare it within the same scope
-let greeting = "say Hi";
-greeting = "say Hello instead";
-XXXXXX aka no go zone
-
-
-var 
-when declared OUTSIDE a function -> GLOBAL 
-var when declare INSIDE a function -> LOCAL to function */
-// promise.then(
-//   (data) => {
-//     console.log(data);
-//   },
-//   (error) => {
-//     console.log("bad request: ", error);
-//   }
-// );
+*/
