92f562044d0d37c3fff1f4bf1949f2a9565d9988
Username <albavmolinanyc@gmail.com>
2022-06-08 16:39:06 -0400

    add form

---
+++ object.js
@@ -0,0 +1,169 @@
+[
+  {
+    meta: {
+      id: "gorgeous",
+      uuid: "ff0112fc-de53-4fd6-85f2-e3080b0899d8",
+      sort: "071883000",
+      src: "collegiate",
+      section: "alpha",
+      stems: ["gorgeous", "gorgeously", "gorgeousness", "gorgeousnesses"],
+      offensive: false,
+    },
+    hwi: {
+      hw: "gor*geous",
+      prs: [
+        {
+          mw: "ˈgȯr-jəs",
+          sound: {
+            audio: "gorgeo01",
+            ref: "c",
+            stat: "1",
+          },
+        },
+      ],
+    },
+    fl: "adjective",
+    def: [
+      {
+        sseq: [
+          [
+            [
+              "sense",
+              {
+                dt: [
+                  [
+                    "text",
+                    "{bc}splendidly or showily {d_link|brilliant|brilliant:1} or {d_link|magnificent|magnificent}",
+                  ],
+                ],
+              },
+            ],
+          ],
+        ],
+      },
+    ],
+    uros: [
+      {
+        ure: "gor*geous*ly",
+        fl: "adverb",
+      },
+      {
+        ure: "gor*geous*ness",
+        fl: "noun",
+      },
+    ],
+    syns: [
+      {
+        pl: "synonyms",
+        pt: [
+          [
+            "text",
+            "{sc}splendid{/sc} {sc}resplendent{/sc} {sc}gorgeous{/sc} {sc}glorious{/sc} {sc}sublime{/sc} {sc}superb{/sc} mean extraordinarily or transcendently impressive. {sc}splendid{/sc} implies outshining the usual or customary. ",
+          ],
+          [
+            "vis",
+            [
+              {
+                t: "the wedding was a {it}splendid{/it} occasion",
+              },
+            ],
+          ],
+          [
+            "text",
+            " {sc}resplendent{/sc} suggests a glowing or blazing splendor. ",
+          ],
+          [
+            "vis",
+            [
+              {
+                t: "{it}resplendent{/it} in her jewelry",
+              },
+            ],
+          ],
+          [
+            "text",
+            " {sc}gorgeous{/sc} implies a rich splendor especially in display of color. ",
+          ],
+          [
+            "vis",
+            [
+              {
+                t: "a {it}gorgeous{/it} red dress",
+              },
+            ],
+          ],
+          [
+            "text",
+            " {sc}glorious{/sc} suggests radiance that heightens beauty or distinction. ",
+          ],
+          [
+            "vis",
+            [
+              {
+                t: "a {it}glorious{/it} sunset",
+              },
+            ],
+          ],
+          [
+            "text",
+            " {sc}sublime{/sc} implies an exaltation or elevation almost beyond human comprehension. ",
+          ],
+          [
+            "vis",
+            [
+              {
+                t: "a vision of {it}sublime{/it} beauty",
+              },
+            ],
+          ],
+          [
+            "text",
+            " {sc}superb{/sc} suggests an excellence reaching the highest conceivable degree. ",
+          ],
+          [
+            "vis",
+            [
+              {
+                t: "her singing was {it}superb{/it}",
+              },
+            ],
+          ],
+        ],
+      },
+    ],
+    quotes: [
+      {
+        t: "Banks of {qword}gorgeous{/qword} flowers were on every hand, and birds with rare and brilliant plumage sang and fluttered in the trees and bushes.",
+        aq: {
+          auth: "L. Frank Baum",
+          source: "{it}The Wonderful Wizard of Oz{/it}",
+          aqdate: "1900",
+        },
+      },
+      {
+        t: "In the Arlberg … my wife and I were taken to the most perfect dinner by an elderly Viennese couple and their {qword}gorgeous{/qword} young daughter, whom we had met in Washington.",
+        aq: {
+          auth: "Geoffrey Wolff",
+          source: "{it}Granta{/it}",
+          aqdate: "Autumn 1990",
+        },
+      },
+      {
+        t: "The difference in appearance can be dramatic, as I saw on a visit to the small, modern factory where Pineider prints its stationery, in a {qword}gorgeous{/qword} part of Tuscany near Florence and bordering Chianti.",
+        aq: {
+          auth: "Corby Kummer",
+          source: "{it}Atlantic{/it}",
+          aqdate: "May 2001",
+        },
+      },
+    ],
+    et: [
+      [
+        "text",
+        "Middle English {it}gorgeouse{/it}, from Middle French {it}gorgias{/it} elegant, perhaps from {it}gorgias{/it} wimple, from {it}gorge{/it} throat",
+      ],
+    ],
+    date: "15th century",
+    shortdef: ["splendidly or showily brilliant or magnificent"],
+  },
+];
--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+const $word = $("#word");
+const $definition = $("#definition");
+const $offensive = $("#offensive"); //this will be a boolean T or F
+const $pronunciation = $("#pronunciation");
 
+/* 
+so the const variables above are constants and exist in the global scope
+# -> allows to select by id 
+$ -> is used through the jQuery install on html head
+*/
+
+// const API_KEY = "cec6fd31-d3a5-454a-a806-f997548b7cbf";
+const promise = $.ajax({
+  url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/ugly?key=cec6fd31-d3a5-454a-a806-f997548b7cbf",
+});
+
+/*
+$.ajax returns a "promise" object, which we are giong to save into the variable promise 
+so promise will be an object that will hold info about the akax request event
+
+the we the success and error callback
+
+ */
+promise.then(
+  (data) => {
+    console.log(data);
+    $word.text(data.word);
+  },
+  (error) => {
+    console.log("bad request: ", error);
+  }
+);
+
+/* 
+const 
+maintain constant values and they can only be accessed within the block they have been declared & it cannot be updated or redeclared within the same scope like let
+
+
+
+
+let 
+is blocked scope, they live within {}, anything within curly braces is a block so a variable avail for use only inside the block it has been declared
+
+XXXXXX aka no go zone you cannot re-declare it within the same scope
+let greeting = "say Hi";
+greeting = "say Hello instead";
+XXXXXX aka no go zone
+
+
+var 
+when declared OUTSIDE a function -> GLOBAL 
+var when declare INSIDE a function -> LOCAL to function */
+// promise.then(
+//   (data) => {
+//     console.log(data);
+//   },
+//   (error) => {
+//     console.log("bad request: ", error);
+//   }
+// );
